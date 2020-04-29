650ee2802a4517c2b247d2ebd7b93ea08e4d2645
mjh0201 <ggb05091@gmail.com>
2020-04-29 11:28:34 +0900

    changed the html for the site

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+#요청을 넘겨 받아 render 메서드 호출하고 받은 return blog/post_list.html 템플릿을 보여즘
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
